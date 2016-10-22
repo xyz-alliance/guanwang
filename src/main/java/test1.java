d3286e959a31dba8826b4adc1cacfaca4d719d19
yang0504 <1347452354@qq.com>
2016-10-22 14:22:50 +0800

    summer-4

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -3,9 +3,7 @@
  */
 public class test1 {
     public static void main(String[] args){
-        System.out.print(11);
         System.out.print(2);
         System.out.print(3);
-        System.out.print(4);
     }
 }
