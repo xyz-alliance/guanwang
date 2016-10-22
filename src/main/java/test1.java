9066a1305c533004e4ed4b4ab6295462340db723
summer128 <hsrjyanglei@163.com>
2016-10-22 15:48:59 +0800

    s1

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -4,9 +4,11 @@
 public class test1 {
     public static void main(String[] args){
         System.out.print(1);
-        System.out.print(2);
         System.out.print(3);
-        System.out.print(5);
-        System.out.print(6);
+        System.out.print('a');
+        System.out.print(3);
+        System.out.print(3);
+        System.out.print(3);
+        System.out.print(3);
     }
 }
